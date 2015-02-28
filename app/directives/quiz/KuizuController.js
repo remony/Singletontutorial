891022928a140eba0c4b63972b0be7af8d93f425
remony <sudouglas@dundee.ac.uk>
2015-02-28 22:16:34 +0000

    You can now load external question json files

--- app/directives/quiz/KuizuController.js
+++ app/directives/quiz/KuizuController.js
@@ -1,21 +1,30 @@
 'use strict';
+
 angular.module('app.kuizu', [])
+
+
     .directive('kuizu', function(kuizuFactory)  {
         return  {
             restrict: 'AE',
             scope:{},
             templateUrl: 'directives/quiz/kuizu.html',
             link: function(scope, elem, attrs) {
+                var filename = attrs.datasource;
+                kuizuFactory.loadQuestions(attrs.datasource);
+                console.log(attrs.datasource);
                 scope.start = function() {
                     scope.id = 0;
                     scope.quizOver = false;
                     scope.inProgress = true;
+
                     scope.getQuestion();
+
                 };
 
                 scope.reset = function() {
                     scope.inProgress = false;
                     scope.score = 0;
+                    scope.totalQuestions = 0;
                 };
 
                 scope.getQuestion = function() {
@@ -31,71 +40,42 @@ angular.module('app.kuizu', [])
                 };
 
                 scope.checkAnswer = function(ans) {
-                    //f(ans.option) return;
-                    //var ans = scope.quizValue;
-                    console.log("You entered: " + ans);
-                    console.log("Answer is: " + scope.options[scope.answer]);
+                    if(ans.option) return;
 
                     if(ans == scope.options[scope.answer]) {
                         scope.score++;
-                        console.log("Hey, you got it right!");
                         scope.correctAns = true;
                     } else {
                         scope.correctAns = false;
-                        console.log("Better luck next time..");
                     }
-
-                    scope.answerMode = false;
-                };
-
-                scope.nextQuestion = function() {
                     scope.id++;
                     scope.getQuestion();
-                }
+                };
 
                 scope.reset();
             }
         };
     })
 
-    .factory('kuizuFactory', function() {
-        var questions = [
-            {
-                question: "2?",
-                options: ["1", "2", "3", "4"],
-                answer: 2
-            },
-            {
-                question: "When did the second world war end?",
-                options: ["1945", "1939", "1944", "1942"],
-                answer: 0
-            },
-            {
-                question: "Which was the first country to issue paper currency?",
-                options: ["USA", "France", "Italy", "China"],
-                answer: 3
-            },
-            {
-                question: "Which city hosted the 1996 Summer Olympics?",
-                options: ["Atlanta", "Sydney", "Athens", "Beijing"],
-                answer: 0
-            },
-            {
-                question: "Who invented telephone?",
-                options: ["Albert Einstein", "Alexander Graham Bell", "Isaac Newton", "Marie Curie"],
-                answer: 1
-            }
-        ];
-
-
+    .factory('kuizuFactory', function($http) {
+        var questions;
 
         return  {
             getQuestion: function(id) {
+
                 if(id < questions.length) {
                     return questions[id];
                 } else {
+
                     return false;
                 }
+            },
+            loadQuestions: function(filename)   {
+                $http.get('directives/quiz/categories/' + filename)
+                    .then(function (res) {
+                        questions = res.data;
+                    });
+
             }
         }
     });

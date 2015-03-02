6578e3812c88df36c8298e3236067a01230bf985
remony <sudouglas@dundee.ac.uk>
2015-03-02 23:56:18 +0000

    Applied dirty method to allow mobiles to see the website how it should be.

--- app/Home/HomeMobileAnimator.js
+++ app/Home/HomeMobileAnimator.js
@@ -20,31 +20,16 @@ var HomeMobileAnimator = function() {
     };
 
     this.backgroundAnimation = {
-        '0'     : { 'background-color'  : '#03A9F4' },
-        '500'   : { 'background-color'  : '#E1F5FE' },
-        '2500'  : { 'background-color'  : '#E1F5FE' },
-        '3000'  : { 'background-color'  : '#FFF8E1' },
-        '4000'  : { 'background-color'  : '#FAFAFA' },
-        ease    :'easeOutQuad'
     };
 
     this.sunAnimation = {
-        ease    : 'easeOutQuad',
-        '20'    : { width :'80vw',  height : '80vw',    top : '10em', left: '10vw', 'background-color' : '#FFFF00', ease: 'easeOutSine' },
-        '130'   : { width :'30vw',  height : '30vw',    top : '9em', left: '35vw', 'background-color' : '#FFFF00', ease: 'easeOutSine'},
-        '1000'  : { width :'3em',   height : '3em',     top : '5em', left: '3vw', 'background-color' : '#FFFF00', ease: 'easeOutSine' },
-        '2000'  : { width :'3em',   height : '3em',     left : '20vw', top: '2em', 'background-color' : '#FFEA00', ease: 'easeOutSine' },
-        '3000'  : { width :'3em',   height : '3em',     left : '50vw', top: '0.5em', 'background-color' : '#F57F17', ease: 'easeOutSine' },
-        '4000'  : { width :'3em',   height : '3em',     left : '60vw', top: '2em', 'background-color' : '#F57F17', opacity : '1.0', ease: 'easeOutSine' },
-        '5000'  : { width :'3em',   height : '3em',     left : '80vw', top : '5em', 'background-color'  : '#263238', opacity : '0.0', ease : 'easeOutSine'
-        }
+
     };
 
     this.titleAnimation = {
         ease:'easeOutQuad',
-        '420': { top: '7em', 'font-size': '1em', color: '#ffffff' },
-        '350': { color: '#5d5d5d' },
-        '200': { top: '5em', 'font-size': '4em', color: '#5d5d5d', ease: 'linear' }
+        '200': { top: '0em', 'font-size': '2em', color: '#ffffff' },
+        '0': { top: '3em', 'font-size': '2em', color: '#5d5d5d', ease: 'linear' }
     };
 
     this.skyAnimation = {
@@ -52,11 +37,7 @@ var HomeMobileAnimator = function() {
             'background-color'  : '#81D4FA',
             opacity             : '0.0'
         },
-        '220'  : {
-            "background-color"  : '#81D4FA',
-            opacity             : '0.0'
-        },
-        '320' : {
+        '200' : {
             "background-color"  : '#81D4FA',
             opacity             : '1.0'
         },
@@ -89,32 +70,32 @@ var HomeMobileAnimator = function() {
 
     this.sectionOne = {
         '0' : {
-            opacity             : '0.0'
+            opacity             : '1.0'
         },
         '400' : {
-            opacity             : '0.0'
+            opacity             : '1.0'
         },
         '600' : {
             opacity             : '1.0'
         },
         '1600' : {
-            opacity             : '0.0'
+            opacity             : '1.0'
         },
         ease  :'easeOutQuad'
     };
 
     this.sectionTwo = {
         '0' : {
-            opacity             : '0.0'
+            opacity             : '1.0'
         },
         '1300' : {
-            opacity             : '0.0'
+            opacity             : '1.0'
         },
         '1600' : {
             opacity             : '1.0'
         },
         '2600' : {
-            opacity             : '0.0'
+            opacity             : '1.0'
         },
         ease  :'easeOutQuad'
     };
@@ -122,58 +103,58 @@ var HomeMobileAnimator = function() {
 
     this.sectionThree = {
         '0' : {
-            opacity             : '0.0'
+            opacity             : '1.0'
         },
         '2300' : {
-            opacity             : '0.0'
+            opacity             : '1.0'
         },
         '2600' : {
             opacity             : '1.0'
         },
         '3600' : {
-            opacity             : '0.0'
+            opacity             : '1.0'
         },
         ease  :'easeOutQuad'
     };
 
     this.sectionFour = {
         '0' : {
-            opacity             : '0.0'
+            opacity             : '1.0'
         },
         '3300' : {
-            opacity             : '0.0'
+            opacity             : '1.0'
         },
         '3600' : {
             opacity             : '1.0'
         },
         '4600' : {
-            opacity             : '0.0'
+            opacity             : '1.0'
         },
         ease  :'easeOutQuad'
     };
 
     this.sectionFive = {
         '0' : {
-            opacity             : '0.0'
+            opacity             : '1.0'
         },
         '4300' : {
-            opacity             : '0.0'
+            opacity             : '1.0'
         },
         '4600' : {
             opacity             : '1.0'
         },
         '5600' : {
-            opacity             : '0.0'
+            opacity             : '1.0'
         },
         ease  :'easeOutQuad'
     };
 
     this.quizAnimation = {
         '0' : {
-            opacity             : '0.0'
+            opacity             : '1.0'
         },
         '5300' : {
-            opacity             : '0.0'
+            opacity             : '1.0'
         },
         '5600' : {
             opacity             : '1.0'
